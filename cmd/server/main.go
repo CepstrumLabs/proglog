6f1c854672d6757b401e9645e1402b6449896e76
Michael Karotsieris <michael.karotsieris@gmail.com>
2022-07-10 11:23:37 +0100

    add log

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"log"
+
 	"github.com/CepstrumLabs/proglog/internal/server"
 )
 
 func main() {
 	srv := server.NewHttpServer(":8080")
+	log.Println("Starting server at localhost:8080 ... ")
 	log.Fatal(srv.ListenAndServe())
 }
