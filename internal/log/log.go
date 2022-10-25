da3f88f2992ccb7af1afd83f2a4c8c42aa71fdba
Michael Karotsieris <michael.karotsieris@gmail.com>
2022-10-26 00:24:50 +0100

    implement Append, Read, Close methods for Log

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,11 +1,19 @@
 package log
 
 import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
+
+	api "github.com/CepstrumLabs/proglog/api/v1"
 )
 
 type Log struct {
-	mu sync.Mutex
+	mu sync.RWMutex
 
 	Dir    string
 	Config Config
@@ -15,5 +23,91 @@ type Log struct {
 }
 
 func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+	return l, l.setup()
+}
 
+func (l *Log) setup() error {
+	files, err := ioutil.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+
+	var baseOffsets []uint64
+
+	for _, file := range files {
+		offStr := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
+		off, _ := strconv.ParseUint(offStr, 10, 10)
+		baseOffsets = append(baseOffsets, off)
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+
+	for i := 0; i < len(baseOffsets); i++ {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+	}
+	if l.segments == nil {
+		if err = l.newSegment(l.Config.Segment.InitialOffset); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	off, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(off + 1)
+	}
+	return off, err
+}
+
+func (l *Log) Read(offset uint64) (*api.Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	var s *segment
+	for _, segment := range l.segments {
+		if segment.baseOffset <= offset && offset < segment.nextOffset {
+			s = segment
+			break
+		}
+	}
+	if s == nil || s.nextOffset <= offset {
+		return nil, fmt.Errorf("offset out of range: %d", offset)
+	}
+
+	return s.Read(offset)
+}
+
+func (l *Log) newSegment(offset uint64) error {
+	return nil
+}
+
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for _, segment := range l.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
